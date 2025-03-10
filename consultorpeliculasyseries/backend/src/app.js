227fbec093b5ef2bf439e80cd879cf1bf86e0f8e
JuanCarlosPerezSanchez <152633822+JuanCarlosPerezSanchez@users.noreply.github.com>
2025-03-10 23:48:06 +0100

    C

--- consultorpeliculasyseries/backend/src/app.js
+++ consultorpeliculasyseries/backend/src/app.js
@@ -10,6 +10,6 @@ const app = express();
 connectDB();
 expressLoader(app);
 
-const PORT = 6000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
