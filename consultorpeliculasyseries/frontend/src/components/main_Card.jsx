a1e4396ef9242af43abbc6fbd29ba6d244073e6b
JuanCarlosPerezSanchez <152633822+JuanCarlosPerezSanchez@users.noreply.github.com>
2025-04-14 20:44:42 +0200

    C

---
+++ consultorpeliculasyseries/frontend/src/components/main_Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContentCard = () => {
+    return (
+        <a href="https://www.justwatch.com/es/serie/adolescencia"><img src="https://images.justwatch.com/poster/328945432/s332/adolescencia.avif, https://images.justwatch.com/poster/328945432/s332/adolescencia.avif" alt="" /></a>
+    );
+};
+
+export default ContentCard;
