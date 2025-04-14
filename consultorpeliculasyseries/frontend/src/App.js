f6947a2a6bbbc17ec64387ac8c34f5eddaa83d13
JuanCarlosPerezSanchez <152633822+JuanCarlosPerezSanchez@users.noreply.github.com>
2025-04-14 21:26:47 +0200

    C

--- consultorpeliculasyseries/frontend/src/App.js
+++ consultorpeliculasyseries/frontend/src/App.js
@@ -3,10 +3,29 @@ import Header from './components/header';
 import './App.css';
 
 function App() {
+  const peliculas = {
+    Peliculas1:{ Nombre:"Harry Potter 1", fechaLanzamiento:"2001" },
+    Peliculas2:{ Nombre:"Harry Potter 2", fechaLanzamiento:"2002" },
+    Peliculas3:{ Nombre:"Harry Potter 3", fechaLanzamiento:"2003" },
+    Peliculas4:{ Nombre:"Harry Potter 4", fechaLanzamiento:"2004" },
+    Peliculas5:{ Nombre:"Harry Potter 5", fechaLanzamiento:"2005" },
+    Peliculas6:{ Nombre:"Harry Potter 6", fechaLanzamiento:"2006" },
+    Peliculas7:{ Nombre:"Harry Potter 7", fechaLanzamiento:"2007" },
+    Peliculas8:{ Nombre:"Harry Potter 8", fechaLanzamiento:"2008" }
+  };
+
   return (
     <div className="App">
       <Header />
-      <ContentCard />
+      <div style={{ display: 'flex' }}>
+        {Object.entries(peliculas).map(([clave, valor]) => (
+          <ContentCard
+            key={clave}
+            titulo={valor.Nombre}
+            anio={valor.fechaLanzamiento}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- consultorpeliculasyseries/frontend/src/components/main_Card.jsx
+++ consultorpeliculasyseries/frontend/src/components/main_Card.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const ContentCard = () => {
+function ContentCard({ titulo, anio }) {
     return (
-        <a href="https://www.justwatch.com/es/serie/adolescencia"><img src="https://images.justwatch.com/poster/328945432/s332/adolescencia.avif, https://images.justwatch.com/poster/328945432/s332/adolescencia.avif" alt="" /></a>
+        <div className="content-card">
+            <h3>{titulo}</h3>
+            <p>Año de lanzamiento: {anio}</p>
+            <a href="https://www.justwatch.com/es/serie/adolescencia"><img src="https://images.justwatch.com/poster/328945432/s332/adolescencia.avif, https://images.justwatch.com/poster/328945432/s332/adolescencia.avif" alt="" /></a>
+        </div>
     );
-};
+}
 
 export default ContentCard;
